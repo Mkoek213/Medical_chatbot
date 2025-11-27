5d0af13feeedb73811f4747f2095cd7c77493b14
Mikolaj <mkolek@student.agh.edu.pl>
2025-11-27 21:52:52 +0100

    implemented app logic

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_pinecone import PineconeVectorStore
+
+from src.helper import download_embeddings
+from src.prompt import system_prompt
+
+app = Flask(__name__)
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+embedding = download_embeddings()
+index_name = "medical-chatbot"
+
+docsearch = PineconeVectorStore.from_existing_index(embedding=embedding, index_name=index_name)
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+chatModel = ChatOpenAI(model="gpt-4o")
+prompt = ChatPromptTemplate.from_messages([("system", system_prompt), ("user", "{input}")])
+
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+@app.route("/")
+def index():
+    return render_template("chat.html")
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": input})
+    print(f"Response: {response['answer']}")
+    return str(response["answer"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
